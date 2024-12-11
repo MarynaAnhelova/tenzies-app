219c4959dcb419d9c9ba3f2629e094d1629b31fa
MarynaAnhelova <marina.anhelova@gmail.com>
2024-12-10 16:40:30 -0800

    Dies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import './index.css';
+import Die from "./Die"
 
-import './App.css';
 
-function App() {
 
+export default function App() {
+    function generateAllNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            const rand = Math.ceil(Math.random() * 6)
+            newDice.push(rand)
+        }
+        return newDice
+    }
+
+    console.log(generateAllNewDice())
+
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+        </main>
+    )
 }
-
-export default App;
